8e37b54802a4bbb123a1c5439bfc7728edb5bc25
gjclark14 <gjclark14@gmail.com>
2020-11-17 07:38:38 -0800

    I think it all works

--- scripts/main.js
+++ scripts/main.js
@@ -44,34 +44,69 @@ if (typeof (document) !== "undefined") {
         // horizontal movements are insertions to the first sting
         // vertical movements are insertions to the second string
 
+        let vstring = "";
+        let hstring = "";
         let r = matrix.rows.length - 1;
+        let R = r;
         let c = matrix.rows[r].cells.length - 1;
+        let C = c;
         matrix.rows[matrix.rows.length - 1].cells[matrix.rows[0].cells.length - 1].style.color = "#83a598";
+        let flagged = 0;
         while (c > 1 && r > 1) {
             // if the indel is the same value as the diag, go diag
             let diag = parseInt(matrix.rows[r - 1].cells[c - 1].innerHTML);
             let horiz = parseInt(matrix.rows[r].cells[c - 1].innerHTML);
             let vert = parseInt(matrix.rows[r - 1].cells[c].innerHTML);
             let min = Math.min(diag, horiz, vert);
-            if (min === diag) {
+            if(r === R && c === C && flagged === 0) {
+                if(vertString.charAt(c - 2) === horizString.charAt(r - 2)) {
+                    matrix.rows[--r].cells[--c].style.color = "#83a598";
+                    vstring += vertString.charAt(c - 1);
+                    hstring += horizString.charAt(r - 1);
+                }
+                flagged = 1;
+            }
+            if (vertString.charAt(c - 1) === horizString.charAt(r - 1) && r < R && c < C) {
                 matrix.rows[--r].cells[--c].style.color = "#83a598";
+                vstring += vertString.charAt(c - 1);
+                hstring += horizString.charAt(r - 1);
+            } else if (min === diag) {
+                matrix.rows[--r].cells[--c].style.color = "#83a598";
+                vstring += vertString.charAt(c-1);
+                hstring += horizString.charAt(r-1);
+
             } else if (min === horiz) {
                 matrix.rows[r].cells[--c].style.color = "#83a598";
+                hstring += "_";
+                vstring += vertString.charAt(c-1);
             } else {
                 // min === vert
                 matrix.rows[--r].cells[c].style.color = "#83a598";
+                vstring += "_";
+                hstring += horizString.charAt(r-1);
             }
         }
         if (r <= 1) {
             while (c > 1) {
                 matrix.rows[r].cells[--c].style.color = "#83a598";
+                hstring += "_";
+                vstring += vertString.charAt(c-1);
             }
         }
         if (c <= 1) {
             while (r > 1) {
                 matrix.rows[--r].cells[c].style.color = "#83a598";
+                vstring += "_";
+                hstring += horizString.charAt(r-1);
             }
         }
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        p1.textContent = vstring.split("").reverse().join("");
+        p2.textContent = hstring.split("").reverse().join("");
+        document.getElementById("tableView").appendChild(p2);
+        document.getElementById("tableView").appendChild(p1);
+
         return matrix;
     }
 
